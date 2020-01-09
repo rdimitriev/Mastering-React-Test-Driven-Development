659ad7b232ee264f30713b778c65aed8e088e0c2
Rosen Dimitriev <rosen.dimitriev@tick42.com>
2020-01-09 16:27:27 +0200

    chapter 2: saving form data

---
+++ appointments/test/customerForm.test.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { createContainer } from './domManipulators'
+import { CustomerForm } from '../src/customerForm'
+
+describe('CustomerForm', () => {
+    let render, container
+    const form = id => container.querySelector(`form[id="${id}"]`)
+
+    beforeEach(() => {
+        ({ render, container } = createContainer())
+    })
+
+    it('renders a form', () => {
+        render(<CustomerForm />)
+        expect(form('customer')).not.toBeNull()
+    })
+
+    const expectToBeInputFieldOfTypeText = formElement => {
+        expect(formElement).not.toBeNull
+        expect(formElement.tagName).toEqual('INPUT')
+        expect(formElement.type).toEqual('text')
+    }
+    const firstNameField = () => form('customer').elements.firstName
+
+    it('renders the first name field as a text box', () => {
+        render(<CustomerForm />)
+        expectToBeInputFieldOfTypeText(firstNameField())
+    })
+
+    it('includes the existing value for the first time', () => {
+        render(<CustomerForm firstName="Ashley" />)
+        expect(firstNameField().value).toEqual('Ashley')
+    })
+
+    const labelFor = formElement => container.querySelector(`label[for="${formElement}"]`)
+    
+    it('renders a label for the first name field', () => {
+        render(<CustomerForm />)
+        expect(labelFor('firstName')).not.toBeNull()
+        expect(labelFor('firstName').textContent).toEqual('First name')
+    })
+
+    it('assigns an id that matches the label id to the first name field', () => {
+        render(<CustomerForm />)
+        expect(firstNameField().id).toEqual('firstName')
+    })
+})
